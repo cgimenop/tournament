3faa09d8e16a26db5418d552a4485f784f8f2539
Jimmy <carlosg204@gmail.com>
2018-12-29 00:47:01 +0100

    Refactored. Fixed file read

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# tournament
-A tournament matcher playtool
+# Tournament
+A Warhammer 40k tournament matcher tool. 
+
++ Only CLI interface
++ Python based (tested with python 2.7)
+
+# Usage
+
++ Edit players.json with player and army information
++ (If necessary) Edit missions.json to add/remove missions form the list
++ Execute '''python matcher.py'''
---
+++ src/matcher.py
@@ -0,0 +1,61 @@
+import json
+import os
+import random
+from pprint import pprint
+
+def load_data(filename):
+
+    if filename is None:
+        raise ValueError("None is not a valid file name")
+
+    with open(os.path.abspath(filename)) as json_file:
+        #raw_data = open(filename).read()
+        
+        return json.load(json_file)
+
+def random_choose(available, key, eliminate=False):
+    candidates = available[key]
+    choosen = random.choice(candidates)
+    if eliminate:
+        candidates.remove(choosen)
+        available[key] = candidates
+    return choosen, available
+
+players = load_data('players.json')
+missions = load_data('missions.json')
+deployments = load_data('deployments.json')
+
+while len(players["players"]) > 1:
+    player1, players = random_choose(players, "players", eliminate=True)
+    player2, players = random_choose(players, "players", eliminate=True)
+    deployment, deployments = random_choose(deployments, "deployments")
+    mission, missions = random_choose(missions, "missions")
+
+    print "Match %(player1)s vs %(player2)s Mission: %(mission)s Map: %(map)s" % \
+    {
+        "player1": player1["name"], 
+        "player2": player2["name"],
+        "mission": mission["name"],
+        "map": deployment["name"]
+    }
+
+consolation_deployment, deployments = random_choose(deployments, "deployments")
+consolation_mission, missions = random_choose(missions, "missions")
+print "Consolation Match Mission: %(mission)s Map: %(map)s" % \
+{
+    "mission": consolation_mission["name"],
+    "map": consolation_deployment["name"]
+}
+
+final_deployment, deployments = random_choose(deployments, "deployments")
+final_mission, missions = random_choose(missions, "missions")
+print "Final Match Mission: %(mission)s Map: %(map)s" % \
+{
+    "mission": final_mission["name"],
+    "map": final_deployment["name"]
+}
+
+
+
+
+
